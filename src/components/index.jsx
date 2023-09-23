a402c8d24d19d2dfdad7218f2cc93ca19d8c6123
trxsh1 <yagosousa630@gmail.com>
2023-09-23 19:19:18 -0300

    Componentização

---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+export const PostCard = ( props ) => {
+    console.log(props)
+    return(
+        <h1>oi</h1>
+    //     <div className='post'>
+    //     <img src={post.cover} alt={post.title}/>
+    //     <div key={post.id} className='post-content'>
+    //       <h1>{post.title}</h1>
+    //       <p>{post.body}</p>
+    //     </div>
+    //   </div>
+    )
+}
